8627094e664f12332fb7770d9e2f40442efb5b72
Alexander Gullen <200480229@ba-a251-2314.student.georgianc.on.ca>
2023-07-20 14:08:51 -0400

    got firebase connection working

--- Bussin/AppDelegate.swift
+++ Bussin/AppDelegate.swift
@@ -10,15 +10,14 @@ import UIKit
 
 import UIKit
 
-import FirebaseCore
+
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Database connect to firebase
+        // connect to firebase
         FirebaseApp.configure()
         
         // Override point for customization after application launch.
