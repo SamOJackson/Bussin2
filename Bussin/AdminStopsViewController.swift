4d2fe37c75c3ddaa2e974b3c5691c3ecc62bff6a
Wayne <quang.ndm91@gmail.com>
2023-07-24 10:20:22 -0400

    added admin page for bus stop

---
+++ Bussin/AdminStopsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AdminStopsViewController.swift
+//  Bussin
+//
+//  Created by Diem Nguyen on 2023-07-24.
+//
+
+import UIKit
+
+class AdminStopsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
