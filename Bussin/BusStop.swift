35fa868df6c4459d5ddf54920420b812b524b33f
Wayne <quang.ndm91@gmail.com>
2023-07-24 10:03:29 -0400

    added FirebaseManager to handle CRUD

---
+++ Bussin/BusStop.swift
@@ -0,0 +1,15 @@
+//
+//  BusStop.swift
+//  Bussin
+//
+//  Created by Diem Nguyen on 2023-07-24.
+//
+
+import Foundation
+
+struct BusStop {
+    let stopId: String
+    let name: String
+    let latitude: Double
+    let longitude: Double
+}
---
+++ Bussin/FirebaseManager.swift
@@ -0,0 +1,81 @@
+//
+//  FirebaseManager.swift
+//  Bussin
+//
+//  Created by Diem Nguyen on 2023-07-24.
+//
+
+import Firebase
+
+class FirebaseManager {
+
+    // ...
+
+    // Create a new bus stop in Firebase Firestore
+    static func createBusStop(busStop: BusStop, completion: @escaping (Error?) -> Void) {
+        let db = Firestore.firestore()
+        let documentRef = db.collection("BusStops").document(busStop.stopId)
+
+        let data: [String: Any] = [
+            "stopId": busStop.stopId,
+            "name": busStop.name,
+            "latitude": busStop.latitude,
+            "longitude": busStop.longitude
+        ]
+
+        documentRef.setData(data) { error in
+            completion(error)
+        }
+    }
+
+    // Read bus stops from Firebase Firestore
+    static func fetchBusStops(completion: @escaping ([BusStop]?, Error?) -> Void) {
+        let db = Firestore.firestore()
+        db.collection("BusStops").getDocuments { (querySnapshot, error) in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+
+            var busStops: [BusStop] = []
+            for document in querySnapshot!.documents {
+                let busStopData = document.data()
+                let stopId = document.documentID
+                let name = busStopData["name"] as? String ?? ""
+                let latitude = busStopData["latitude"] as? Double ?? 0.0
+                let longitude = busStopData["longitude"] as? Double ?? 0.0
+                let busStop = BusStop(stopId: stopId, name: name, latitude: latitude, longitude: longitude)
+                busStops.append(busStop)
+            }
+
+            completion(busStops, nil)
+        }
+    }
+
+    // Update a bus stop in Firebase Firestore
+    static func updateBusStop(busStop: BusStop, completion: @escaping (Error?) -> Void) {
+        let db = Firestore.firestore()
+        let documentRef = db.collection("BusStops").document(busStop.stopId)
+
+        let data: [String: Any] = [
+            "stopId": busStop.stopId,
+            "name": busStop.name,
+            "latitude": busStop.latitude,
+            "longitude": busStop.longitude
+        ]
+
+        documentRef.setData(data) { error in
+            completion(error)
+        }
+    }
+
+    // Delete a bus stop from Firebase Firestore
+    static func deleteBusStop(stopId: String, completion: @escaping (Error?) -> Void) {
+        let db = Firestore.firestore()
+        let documentRef = db.collection("BusStops").document(stopId)
+
+        documentRef.delete { error in
+            completion(error)
+        }
+    }
+}
